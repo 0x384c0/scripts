82b62ace7bcdcfba69b9bd4e3d2a1805ade8eca7
0x384c0 <0x384c0@gmail.com>
2021-06-09 09:54:31 +0600

    updated to mp3

--- to_mp3_recursively.sh
+++ to_mp3_recursively.sh
@@ -1,11 +1,11 @@
 shopt -s nullglob
 set -e
 
-for file in *.{aac,wav}; do
+for file in *.{aac,wav,opus}; do
 	echo $file
-	base_name=$(basename $file)
+	base_name=$(basename "$file")
 	file_name="${base_name%.*}"
-	ffmpeg -hide_banner -loglevel panic -i "$file" -ac 2 -b:a 320k  -f mp3 $(dirname $file)/$file_name".mp3"; 
-	mkdir -p trash/$(dirname $file)
-	mv "$file" trash/$file
+	ffmpeg -hide_banner -loglevel panic -i "$file" -ac 2 -b:a 320k  -f mp3 "$(dirname "$file")/$file_name.mp3";
+	mkdir -p "trash/$(dirname "$file")"
+	mv "$file" "trash/$file"
 done
