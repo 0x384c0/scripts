415039aab9a34d4a4032537bc094a7bc0cda3f63
0x384c0 <0x384c0@gmail.com>
2021-08-27 19:06:13 +0600

    updated setup script

--- other/radio_alarm/setup_alarm.sh
+++ other/radio_alarm/setup_alarm.sh
@@ -1,2 +1,4 @@
-echo "0 3 * * * sh $PWD/start_radio_session.sh" | xsel -b
-EDITOR=nano crontab -e
+CRON_LINE="0 3 * * * sh $PWD/start_radio_session.sh"
+echo "$CRON_LINE"
+(crontab -u $(whoami) -l; echo "$CRON_LINE" ) | crontab -u $(whoami) -
+crontab -e
--- other/radio_alarm/start_radio.sh
+++ other/radio_alarm/start_radio.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export XDG_RUNTIME_DIR=/run/user/1000 
 cd "${0%/*}"
 sh smooth_volume_increase.sh &
-ffplay -nodisp -volume 5 http://anon.fm:8000/radio
+ffplay -hide_banner -nodisp -volume 5 http://anon.fm:8000/radio
+# cvlc http://anon.fm:8000/radio
