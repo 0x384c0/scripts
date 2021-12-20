b810e37469659f17b42ffbbc725a58ad84956867
0x384c0 <0x384c0@gmail.com>
2021-12-20 18:00:52 +0600

    lowered volume

--- other/radio_alarm/start_radio.sh
+++ other/radio_alarm/start_radio.sh
@@ -2,5 +2,5 @@
 export XDG_RUNTIME_DIR=/run/user/1000 
 cd "${0%/*}"
 sh smooth_volume_increase.sh &
-ffplay -hide_banner -nodisp -volume 5 http://anon.fm:8000/radio
+ffplay -hide_banner -nodisp -volume 3 http://anon.fm:8000/radio
 # cvlc http://anon.fm:8000/radio
