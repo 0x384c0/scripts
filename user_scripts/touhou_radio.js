9cc1f037e3c1a87a8386cb178746f5879ccb69a5
0x384c0 <0x384c0@gmail.com>
2023-10-22 10:04:12 +0600

    refactor

--- user_scripts/touhou_radio.js
+++ user_scripts/touhou_radio.js
@@ -9,37 +9,37 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+    'use strict'
 
-    class RandomPlayer{
-        host = "/tlmc/";
-        soundsFile = "/otokei.json";
-        soundsDatabaseKey = 'soundsList';
+    class RandomPlayer {
+        host = "/tlmc/"
+        soundsFile = "/otokei.json"
+        soundsDatabaseKey = 'soundsList'
 
         //Utils
         getRandomInt(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min) + min);
+            min = Math.ceil(min)
+            max = Math.floor(max)
+            return Math.floor(Math.random() * (max - min) + min)
         }
 
         isJsonString(str) {
             try {
-                JSON.parse(str);
+                JSON.parse(str)
             } catch (e) {
-                return false;
+                return false
             }
-            return true;
+            return true
         }
 
         // Player
-        musicPlayer;
-        currentSong = -1;
+        musicPlayer
+        currentSong = -1
 
         setSong(index) {
-            this.currentSong = index;
-            this.musicPlayer.src = this.getSongUrl(this.currentPlaylist[index]);
+            this.currentSong = index
+            this.musicPlayer.src = this.getSongUrl(this.currentPlaylist[index])
             this.updateMusicPlayerSong(index)
         }
 
@@ -48,136 +48,136 @@
         }
 
         initPlayer() {
-            let randomPlayer = this;
-            this.musicPlayer = document.createElement("audio");
-            this.musicPlayer.controls = true;
-            this.musicPlayer.volume = 0.4;
-            this.musicPlayer.preload = true;
-            this.musicPlayer.onended = function() {
-                randomPlayer.setSong(randomPlayer.getNextSongId());
-                randomPlayer.musicPlayer.play();
-            };
-            document.body.innerHTML = '';
-            document.body.append(`${this.currentPlaylist.length} songs`);
-            document.body.appendChild(document.createElement("br"));
-            document.body.appendChild(this.musicPlayer);
-            this.setSong(this.getNextSongId());
+            let randomPlayer = this
+            this.musicPlayer = document.createElement("audio")
+            this.musicPlayer.controls = true
+            this.musicPlayer.volume = 0.4
+            this.musicPlayer.preload = true
+            this.musicPlayer.onended = function () {
+                randomPlayer.setSong(randomPlayer.getNextSongId())
+                randomPlayer.musicPlayer.play()
+            }
+            document.body.innerHTML = ''
+            document.body.append(`${this.currentPlaylist.length} songs`)
+            document.body.appendChild(document.createElement("br"))
+            document.body.appendChild(this.musicPlayer)
+            this.setSong(this.getNextSongId())
         }
 
         //Data
-        soundsDatabase = [];
-        currentPlaylist = [];
+        soundsDatabase = []
+        currentPlaylist = []
         initListing() {
-            this.currentPlaylist = [];
-            this.traverse(this.soundsDatabase,[],this.currentPlaylist);
+            this.currentPlaylist = []
+            this.traverse(this.soundsDatabase, [], this.currentPlaylist)
         }
 
-        findAudioFiles(keys, value, playlist){
-            let lastKey = keys.slice(-1)[0];
-            let pathKeys = keys.slice(0, -1);
+        findAudioFiles(keys, value, playlist) {
+            let lastKey = keys.slice(-1)[0]
+            let pathKeys = keys.slice(0, -1)
             let divider = "/"
-            let path = pathKeys.join(divider);
-            if (lastKey == "files" && Array.isArray(value.files)){
-                playlist.push(...(value.files.map(file => `${path}${divider}${file}`)));
-                return true;
+            let path = pathKeys.join(divider)
+            if (lastKey == "files" && Array.isArray(value.files)) {
+                playlist.push(...(value.files.map(file => `${path}${divider}${file}`)))
+                return true
             } else {
-                return false;
+                return false
             }
         }
 
-        traverse(obj,keys,playlist){
+        traverse(obj, keys, playlist) {
             for (var key in obj) {
-                let nextObject = obj[key];
-                let isObject = nextObject !== null && typeof(nextObject) == "object";
-                keys.push(key);
-                let isNotAudioFiles = !this.findAudioFiles(keys,obj,playlist);
+                let nextObject = obj[key]
+                let isObject = nextObject !== null && typeof (nextObject) == "object"
+                keys.push(key)
+                let isNotAudioFiles = !this.findAudioFiles(keys, obj, playlist)
 
-                if (isObject && isNotAudioFiles){
-                    this.traverse(nextObject,keys,playlist);
+                if (isObject && isNotAudioFiles) {
+                    this.traverse(nextObject, keys, playlist)
                 }
-                keys.pop();
+                keys.pop()
             }
         }
 
-        getNextSongId(){
-            return this.getRandomInt(0,this.currentPlaylist.length - 1)
+        getNextSongId() {
+            return this.getRandomInt(0, this.currentPlaylist.length - 1)
         }
 
-        getListFromCache(){
+        getListFromCache() {
             return new Promise((resolve, reject) => {
-                let data = localStorage.getItem(this.soundsDatabaseKey);
-                if (data != null && this.isJsonString(data)){
-                    resolve(data);
+                let data = localStorage.getItem(this.soundsDatabaseKey)
+                if (data != null && this.isJsonString(data)) {
+                    resolve(data)
                 } else {
-                    localStorage.removeItem(this.soundsDatabaseKey);
-                    resolve(null);
+                    localStorage.removeItem(this.soundsDatabaseKey)
+                    resolve(null)
                 }
-            });
+            })
         }
 
-        async getListFromWeb(){
-            const response = await fetch(this.soundsFile);
-            console.log(this.soundsFile);
-            console.log(response);
-            const data = await response.text();
-            localStorage.removeItem(this.soundsDatabaseKey);
-            localStorage.setItem(this.soundsDatabaseKey, data);
-            return data;
+        async getListFromWeb() {
+            const response = await fetch(this.soundsFile)
+            console.log(this.soundsFile)
+            console.log(response)
+            const data = await response.text()
+            localStorage.removeItem(this.soundsDatabaseKey)
+            localStorage.setItem(this.soundsDatabaseKey, data)
+            return data
         }
 
-        async downloadList(){
-            var data = await this.getListFromCache();
-            if (data == null){
-                data = await this.getListFromWeb();
+        async downloadList() {
+            var data = await this.getListFromCache()
+            if (data == null) {
+                data = await this.getListFromWeb()
             }
-            this.soundsDatabase = JSON.parse(data);
+            this.soundsDatabase = JSON.parse(data)
         }
 
         // UI
         updateMusicPlayerSong(index) {
-            var current = this.currentPlaylist[index];
-            document.body.appendChild(document.createElement("br"));
-            document.body.append(current);
+            var current = this.currentPlaylist[index]
+            document.body.appendChild(document.createElement("br"))
+            document.body.append(current)
         }
 
-        async init(){
-            await this.downloadList();
-            this.initListing();
-            this.initPlayer();
+        async init() {
+            await this.downloadList()
+            this.initListing()
+            this.initPlayer()
         }
     }
 
     class PlaylistGenerator {
         // Playlist
         initSavePlaylistButton() {
-            const button = document.createElement('button');
-            button.textContent = 'Save as Playlist';
+            const button = document.createElement('button')
+            button.textContent = 'Save as Playlist'
             button.addEventListener('click', () => {
-                this.generateAndDownloadM3U8(this.getUrls(), "Touhou things.m3u8");
-            });
-            document.body.appendChild(document.createElement("br"));
-            document.body.appendChild(button);
+                this.generateAndDownloadM3U8(this.getUrls(), "Touhou things.m3u8")
+            })
+            document.body.appendChild(document.createElement("br"))
+            document.body.appendChild(button)
         }
 
         generateAndDownloadM3U8(urls, filename) {
             // Create the M3U8 content by joining the URLs with appropriate M3U8 format
-            const m3u8Content = "#EXTM3U\n" + urls.map(url => `#EXTINF:-1,Video\n${url}`).join("\n");
-            const blob = new Blob([m3u8Content], { type: "application/vnd.apple.mpegurl" });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = filename;
-            a.click();
-            URL.revokeObjectURL(url);
+            const m3u8Content = "#EXTM3U\n" + urls.map(url => `#EXTINF:-1,Video\n${url}`).join("\n")
+            const blob = new Blob([m3u8Content], { type: "application/vnd.apple.mpegurl" })
+            const url = URL.createObjectURL(blob)
+            const a = document.createElement("a")
+            a.href = url
+            a.download = filename
+            a.click()
+            URL.revokeObjectURL(url)
         }
 
         getUrls() {
             return this.player.currentPlaylist
-                .map((name) => {return this.player.getSongUrl(name)})
+                .map((name) => { return this.player.getSongUrl(name) })
                 .map((path) => { return window.location.origin + path })
         }
 
-        init(player){
+        init(player) {
             this.player = player
             this.initSavePlaylistButton()
         }
@@ -189,4 +189,4 @@
     player
         .init()
         .then(() => { playlistGenerator.init(player) })
-})();
+})()
