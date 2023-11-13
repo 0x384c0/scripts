ec5bd2f3ea62bff749b1745042c1e0d8ca144946
0x384c0 <0x384c0@gmail.com>
2023-11-13 15:01:48 +0600

    added title extraction script

---
+++ other/KCS2_playlist/extract_title_from_wiki.js
@@ -0,0 +1,51 @@
+function extractTableData() {
+  const tables = document.querySelectorAll('table');
+
+  const resultMap = new Map();
+
+  tables.forEach(table => {
+    const rows = table.querySelectorAll('tr');
+
+    selectedColumn = undefined
+    rows.forEach(row => {
+        const headers = Array.from(row.querySelectorAll('th')).map(element => element.textContent.trim());
+        const rowData = Array.from(row.querySelectorAll('td')).map(element => element.textContent.trim());
+        const rowIds = Array.from(row.querySelectorAll('td')).map(element => element.id);
+        if (headers[0] === "-"){
+            const header = headers.concat(rowData)
+            selectedColumn = header.indexOf("English")
+            if (selectedColumn == -1){
+                selectedColumn = header.indexOf("Source")
+            }
+        } else if (selectedColumn && rowIds[0]){
+            resultMap[rowIds[0].toLowerCase()] = rowData[selectedColumn];
+        }
+    })
+  });
+  return resultMap;
+}
+
+function mapToCsv(map) {
+    let csvString = '';
+  
+    map.forEach((value, key) => {
+        console.log(value);
+      csvString += `${key},${value}\n`;
+    });
+  
+    return csvString;
+  }
+
+  function downloadCsv(map, filename) {
+    const csvString = mapToCsv(map);
+    const blob = new Blob([csvString], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+  }
+
+map = extractTableData()
+console.log(mapToCsv(map))
+// downloadCsv(map, 'output.csv');
+
