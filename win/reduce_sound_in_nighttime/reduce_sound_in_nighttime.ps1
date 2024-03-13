8eac3b6b23180988fdb86dee13790ae984939dcd
0x384c0 <0x384c0@gmail.com>
2024-03-13 16:25:51 +0600

    added sound controller

---
+++ win/reduce_sound_in_nighttime/reduce_sound_in_nighttime.ps1
@@ -0,0 +1,27 @@
+# Function to get the sound volume percentage
+function Get-SoundVolume {
+    $output = & svcl /Stdout /GetPercent Speakers
+    return [double]::Parse($output)
+}
+
+# Function to set the sound volume to a specified percentage
+function Set-SoundVolume {
+    echo "Set the sound volume to 10%"
+    & svcl /SetVolume "Speakers" 2
+}
+
+# Function to set the sound volume to a specified percentage
+function Set-SoundVolumeIfNeeded {
+    # Check if the sound volume is greater than 10%
+    $currentVolume = Get-SoundVolume
+    echo "v: " $currentVolume
+    if ($currentVolume -gt 2) {
+        Set-SoundVolume
+    }
+}
+
+# Check if the current time is between 22:00 and 04:00
+$currentHour = (Get-Date).Hour
+if ($currentHour -ge 22 -or $currentHour -lt 04) {
+    Set-SoundVolumeIfNeeded
+}
