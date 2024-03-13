eedc51f6710fc28afe49990b81c5017010c2957c
0x384c0 <0x384c0@gmail.com>
2024-03-13 16:25:51 +0600

    added sound controller

---
+++ win/reduce_sound_in_nighttime/create_task.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set "taskName=ReduceSoundInNighttime"
+set "scriptPath=%~dp0reduce_sound_in_nighttime.ps1"
+set "userLogonTrigger=UserLogon"
+set "repeatInterval=30"
+
+schtasks /Create /TN "%taskName%" /TR "powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -File \"%scriptPath%\"" /SC ONLOGON /RU "%username%" /RL HIGHEST /F
+
+control schedtasks
+
+pause
