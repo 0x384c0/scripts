e2420714e01f1c62dde053c0ae46344a856143cb
0x384c0 <0x384c0@gmail.com>
2024-03-15 18:00:18 +0600

    updated task script

--- win/reduce_sound_in_nighttime/create_task.bat
+++ win/reduce_sound_in_nighttime/create_task.bat
@@ -5,7 +5,7 @@ set "scriptPath=%~dp0reduce_sound_in_nighttime.ps1"
 set "userLogonTrigger=UserLogon"
 set "repeatInterval=30"
 
-schtasks /Create /TN "%taskName%" /TR "powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -File \"%scriptPath%\"" /SC ONLOGON /RU "%username%" /RL HIGHEST /F
+schtasks /Create /TN "%taskName%" /TR "powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -File \"%scriptPath%\"" /SC ONLOGON /RU System /RL HIGHEST /F
 
 control schedtasks
 
