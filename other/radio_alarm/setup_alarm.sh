6ac058bf40e033cad80ffbe8d2b78525d4bbd4cc
0x384c0 <0x384c0@gmail.com>
2024-07-14 10:58:04 +0600

    updated radio script

--- other/radio_alarm/setup_alarm.sh
+++ other/radio_alarm/setup_alarm.sh
@@ -1,4 +1,4 @@
-CRON_LINE="0 3 * * * sh $PWD/start_radio_session.sh"
+CRON_LINE="0 7 * * * sh $PWD/start_radio_session.sh"
 echo "$CRON_LINE"
 (crontab -u $(whoami) -l; echo "$CRON_LINE" ) | crontab -u $(whoami) -
 crontab -e
