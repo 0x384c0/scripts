0e53d84e4307e14586c6470b42faccf3fe3d9003
0x384c0 <0x384c0@gmail.com>
2024-07-14 11:43:27 +0600

    updated radio setup

--- other/radio_alarm/setup_alarm.sh
+++ other/radio_alarm/setup_alarm.sh
@@ -1,4 +1,14 @@
+# dependecies
+sudo apt-get install tmux ffmpeg
+
+# alarm
 CRON_LINE="0 7 * * * sh $PWD/start_radio_session.sh"
 echo "$CRON_LINE"
 (crontab -u $(whoami) -l; echo "$CRON_LINE" ) | crontab -u $(whoami) -
 crontab -e
+
+# aliases
+touch ~/.bash_aliases
+
+echo "alias vold='sh $PWD/volume_down.sh'" >> ~/.bash_aliases
+echo "alias volu='sh $PWD/volume_up.sh'" >> ~/.bash_aliases
