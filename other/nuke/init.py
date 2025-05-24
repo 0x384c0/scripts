5862730946ed28d0cdf4d75fb5a648a4e8cc7821
0x384c0 <0x384c0@gmail.com>
2025-05-24 16:45:55 +0600

    updated nuke

--- other/nuke/init.py
+++ other/nuke/init.py
@@ -1,2 +1,9 @@
-nuke.pluginAddPath('./gizmos' )
-nuke.pluginAddPath('./gizmos/WPTK_20150325/_gizmos' )
+import os
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+nuke.pluginAddPath(os.path.join(dirname, 'gizmos'))
+
+dirname, filename = os.path.split(os.path.abspath(__file__))
+
+for root, dirs, files in os.walk(dirname):
+    nuke.pluginAddPath(root)
--- other/nuke/menu.py
+++ other/nuke/menu.py
@@ -1,21 +1,23 @@
 import nuke
+import os
 
 toolbar = nuke.toolbar("Nodes")
-
 gizmoskMenu = toolbar.addMenu('Custom Gizmos')
-gizmoskMenu.addCommand('expoglow', 'nuke.createNode("expoglow")') 
-gizmoskMenu.addCommand('FireflyKiller', 'nuke.createNode("FireflyKiller")') 
-gizmoskMenu.addCommand('FlareFactory', 'nuke.createNode("FlareFactory")') 
-gizmoskMenu.addCommand('K_LensEngine', 'nuke.createNode("K_LensEngine")') 
-gizmoskMenu.addCommand('fl_VirtualLens', 'nuke.createNode("fl_VirtualLens")') 
-gizmoskMenu.addCommand('SliceTool', 'nuke.createNode("SliceTool")') 
-gizmoskMenu.addCommand('vignette_v2', 'nuke.createNode("vignette_v2")')
 
-wptkMenu = gizmoskMenu.addMenu('World Position Tool Kit')
-wptkMenu.addCommand('Mask3DCubical_ik', 'nuke.createNode("Mask3DCubical_ik")') 
-wptkMenu.addCommand('Mask3DGradient_ik', 'nuke.createNode("Mask3DGradient_ik")') 
-wptkMenu.addCommand('Mask3DSpherical_ik', 'nuke.createNode("Mask3DSpherical_ik")') 
-wptkMenu.addCommand('TransformWorld_ik', 'nuke.createNode("TransformWorld_ik")') 
-wptkMenu.addCommand('WorldPos_Texture_Projection_ik', 'nuke.createNode("WorldPos_Texture_Projection_ik")') 
-wptkMenu.addCommand('WorldPos_Texture_Generator_ik', 'nuke.createNode("WorldPos_Texture_Generator_ik")') 
-wptkMenu.addCommand('WorldPos_Lambert_Shader_ik', 'nuke.createNode("WorldPos_Lambert_Shader_ik")')
+dirname = os.path.dirname(os.path.abspath(__file__))
+pluginPath = os.path.join(dirname, 'gizmos')
+
+def add_gizmos_to_menu(menu, folder):
+    for entry in sorted(os.listdir(folder)):
+        path = os.path.join(folder, entry)
+        if os.path.isdir(path):
+            submenu = menu.addMenu(entry)
+            add_gizmos_to_menu(submenu, path)
+        elif entry.endswith('.gizmo'):
+            gizmo_name = os.path.splitext(entry)[0]
+            menu.addCommand(gizmo_name, f'nuke.createNode("{gizmo_name}")')
+
+if os.path.exists(pluginPath):
+    add_gizmos_to_menu(gizmoskMenu, pluginPath)
+else:
+    nuke.message(f"Gizmos folder not found: {pluginPath}")
