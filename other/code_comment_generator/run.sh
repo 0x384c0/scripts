f431603987ff7411d3b9be3def81c4dd8d0e9c12
0x384c0 <0x384c0@gmail.com>
2026-07-10 22:55:08 +0600

    cleanup old code

--- other/code_comment_generator/run.sh
+++
@@ -1,2 +0,0 @@
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-node $parent_path"/src/index.js" $1
--- other/code_comment_generator/run_diff_kotlin.sh
+++
@@ -1,9 +0,0 @@
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-IFS=$'\n'
-files=($(git diff --name-only develop | grep -E "\.kt$"))
-
-for file in "${files[@]}"
-do
-    sh $parent_path"/run.sh" $file
-done
