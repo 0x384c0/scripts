3db3488d56b0e8cf69fecf7d5a012c69998806ad
0x384c0 <0x384c0@gmail.com>
2026-07-10 22:53:18 +0600

    remove --build flag from docker compose command in start.bat

--- AI/ComfyUI_docker/docker/start.bat
+++ AI/ComfyUI_docker/docker/start.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 IF EXIST G:\AI\comfy\ (
     echo G: drive found, mounting extra volumes...
-    docker compose -f docker-compose.yml -f docker-compose.gdrive.yml up -d --build
+    docker compose -f docker-compose.yml -f docker-compose.gdrive.yml up -d
 ) ELSE (
     echo G: drive not found, starting without extra mounts...
     docker compose up  -d
